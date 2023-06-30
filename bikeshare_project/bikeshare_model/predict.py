dcbf3426b38896633e0ce7fb47d6c22e4ef9e92e
Giri Prathivadi <giri.prathivadi@gmail.com>
2023-06-30 12:20:40 +0530

    Uncommented the output statements

--- bikeshare_project/bikeshare_model/predict.py
+++ bikeshare_project/bikeshare_model/predict.py
@@ -39,9 +39,9 @@ def make_prediction(*, input_data: Union[pd.DataFrame, dict]) -> dict:
 
 
 
-#if __name__ == "__main__":
+if __name__ == "__main__":
 
-#    data_in = {'dteday': ['2012-11-6'], 'season': ['winter'], 'hr': ['6pm'], 'holiday': ['No'], 'weekday': ['Tue'],
-#               'workingday': ['Yes'], 'weathersit': ['Clear'], 'temp': [16], 'atemp': [17.5], 'hum': [30], 'windspeed': [10]}
+    data_in = {'dteday': ['2012-11-6'], 'season': ['winter'], 'hr': ['6pm'], 'holiday': ['No'], 'weekday': ['Tue'],
+               'workingday': ['Yes'], 'weathersit': ['Clear'], 'temp': [16], 'atemp': [17.5], 'hum': [30], 'windspeed': [10]}
 
-#    make_prediction(input_data = data_in)
+    make_prediction(input_data = data_in)
