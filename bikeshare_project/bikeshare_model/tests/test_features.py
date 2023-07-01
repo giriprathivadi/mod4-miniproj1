bce2a771e0ad73c61d121632291660ec217c6992
Giri Prathivadi <128894310+giriprathivadi@users.noreply.github.com>
2023-07-01 08:19:04 +0530

    Update test_features.py

--- bikeshare_project/bikeshare_model/tests/test_features.py
+++ bikeshare_project/bikeshare_model/tests/test_features.py
@@ -2,24 +2,24 @@
 Note: These tests will fail if you have not first trained the model.
 """
 
-import sys
+import sys, os
 from pathlib import Path
-sys.path.append(str(Path(__file__).parent.parent))
+
+# windows changes
+# sys.path.append(str(Path(__file__).parent.parent))
+# Linux changes
+file = Path(__file__).resolve()
+print("file:", file)
+parent, root = file.parent, file.parents[1]
+sys.path.append(str(root))
+
+print("paraen:", file.parents[1])
+print("paraen:", file.parent)
 
 import numpy as np
 from bikeshare_model.config.core import config
-from bikeshare_model.processing.features import age_col_tfr
-
-
-def test_age_variable_transformer(sample_input_data):
-    # Given
-    transformer = age_col_tfr(
-        variables=config.model_config.age_var,  # cabin
-    )
- #   assert np.isnan(sample_input_data.loc[709,'Age'])
-
-    # When
-    subject = transformer.fit(sample_input_data).transform(sample_input_data)
-
-    # Then
-  #  assert subject.loc[709,'Age'] == 21
+from bikeshare_model.processing.features import (
+    Mapper,
+    WeathersitImputer,
+    WeekdayImputer,
+)
